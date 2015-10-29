ad035c418e606cd4d600b885be64f0884d0bc151
ssyl55 <stliu.webservices@gmail.com>
2015-10-29 21:10:02 +0800

    Trying to set the preferred size of the Frame again.

--- IA-stephen-conways-game-of-life/src/conways/game/of/life/ConwaysGameOfLife.java
+++ IA-stephen-conways-game-of-life/src/conways/game/of/life/ConwaysGameOfLife.java
@@ -64,6 +64,8 @@ public class ConwaysGameOfLife extends javax.swing.JFrame
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Conway's Game of Life");
+        setPreferredSize(new java.awt.Dimension(960, 720));
+        setResizable(false);
         setSize(new java.awt.Dimension(960, 720));
 
         rootPanel.setToolTipText("");
