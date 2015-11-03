99035e1a0a01c8355e669b745980be34a1fad29b
ssyl55 <stliu.webservices@gmail.com>
2015-11-03 15:19:31 +0800

    test change.

--- IA-stephen-conways-GoL/src/conways/game/of/life/Cell.java
+++ IA-stephen-conways-GoL/src/conways/game/of/life/Cell.java
@@ -9,7 +9,8 @@ public class Cell
     private int row;
     private int col;
     private int state;
-        
+    private int test;
+    
     /**
      * Initializes the cell object with position and default off state.
      * @param row the row position of the cell.
